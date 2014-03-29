8d8cb014c585a528a498ac433319bc834c3a8ccf
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-03-29 14:00:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VOSShoppingList::Application.config.secret_key_base = 'c3b27c113db9d30d3d64af6eec19b97db0244c1da204d6a6ef8c11efbc61d5d2845b2a7066e8f45fadc53da04a5076c7fa2466fa9f17b256ecdc70de5fd4974b'
