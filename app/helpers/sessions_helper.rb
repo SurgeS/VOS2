36418ea18aaba63628eaaaac4fd3bbed1a04c37e
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-04-08 00:19:54 +0200

    Finish user area

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,10 @@ module SessionsHelper
     token = User.hash(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: token)
   end
+
+  def sign_out
+    current_user.update_attribute(:remember_token, User.hash(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
 end
