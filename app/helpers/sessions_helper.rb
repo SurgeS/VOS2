3cecafb7e3db2b46072ae11b4c18ba5a8ec10e78
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-04-08 00:07:40 +0200

    add sign in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,21 @@
 module SessionsHelper
+  def sign_in(user)
+    token = User.new_remember_token
+    cookies.permanent[:remember_token] = token
+    user.update_attribute(:remember_token, User.hash(token))
+    self.current_user = user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    token = User.hash(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: token)
+  end
 end
