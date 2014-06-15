8b32120c990014d62edaa8927f6b261a95a3415f
Lukas Miskovsky <miskovskylukas@gmail.com>
2014-06-15 10:43:25 +0200

    Simple refactoring

--- app/models/shoplist.rb
+++ app/models/shoplist.rb
@@ -4,4 +4,40 @@ class Shoplist < ActiveRecord::Base
 
   has_many :item_in_lists, dependent: :destroy
   has_many :products, through: :item_in_lists
+
+  def make_order products
+    shops = ["Billa", "Carrefour", "Kaufland", "Lidl", "Tesco"]
+#map it shops na ceny + vyratat average
+#vo view uz len preiterovat
+
+    ordProducts = Hash.new
+
+    products.each do |product|      #pre kazdy produkt
+      prices = product.prices       #get jeho zname ceny
+
+      ordPrices = ActiveSupport::OrderedHash.new   #vytvor ciast. objekt na vopchatie usortenych cien/shop
+
+      (0..shops.length-1).each do |index|        #prejdi vsetky shopy
+        done = false
+        sum = 0
+        prices.each do |price|      #prejdi vsetky ceny a najdi taku s akt. shopom
+          sum += price.price
+          if(price.shop == shops[index])
+            done = true
+            ordPrices[price.shop] = price.price#daj do OrderedHash
+          end
+        end
+        if(!done)
+          if(prices.length==0)
+            ordPrices[shops[index]] = '-'
+          else
+            cena = sum/prices.length #ak nie je v db, daj tam avg vsetkych
+            ordPrices[shops[index]] = sprintf('%.2f', cena)
+          end
+        end
+      end
+      ordProducts[product.name] = ordPrices
+    end
+    ordProducts
+  end
 end
--- lib/tasks/crawler.rb
+++ lib/tasks/crawler.rb
@@ -2,95 +2,41 @@
 require 'open-uri'
 require 'nokogiri'
 
-#id = 2
-
-
-#zlacnene.sk
-#(1..56).each do |id|
-#  puts "Strana cislo #{id}"
-#  html = open("http://www.zlacnene.sk/tovar/hladaj/sk-potraviny/p/#{id}")
-#  doc = Nokogiri::HTML(html)
-#  doc.search('.zboziVypis').each do |produkt|
-#    meno = produkt.search('h2 a').text #.split
-#    #meno.sub!(/\d.*/im, "") --> ostranenie gramaze, objemu a pod za nazvom produktu
-#    puts meno
-#
-#
-#    cena = produkt.search('.cena').text.split(' ')
-#    obchod = produkt.search('.prodejnaName').text
-#    platnostDo = produkt.search('.platiDo').text
-#    puts cena[1] +" "+obchod +" "+ platnostDo
-#
-#    puts ""
-#
-#    end
-#end
-
-#zlacnene.sk
-#Price.delete_all "person_id = 5 AND (category = 'Something' OR category = 'Else')"
-#(1..56).each do |id|
-#  #puts "Strana cislo #{id}"
-#  html = open("http://www.zlacnene.sk/tovar/hladaj/sk-potraviny/p/#{id}")
-#  doc = Nokogiri::HTML(html)
-#  doc.search('.zboziVypis').each do |produkt|
-#    meno = produkt.search('h2 a').text
-#    #puts meno
-#    cena = produkt.search('.cena').text.split(' ')
-#    obchod = produkt.search('.prodejnaName').text
-#    platnostDo = produkt.search('.platiDo').text
-#    # puts cena[1] +" "+obchod +" "+ platnostDo
-#    temp = cena[1].sub(',','.').to_f
-#
-#    puts cena[1] + " "+obchod +" "+ platnostDo
-#    #item = Product.find_by(name: meno)
-#    #if item.nil? then
-#    #  item = Product.create(name: meno)
-#    #  item.prices.create!(price: temp, shop: obchod)
-#    #else
-#    #  if item.prices.where("price = ? AND shop = ?", temp, obchod).nil?
-#    #    item.prices.create!(price: temp, shop: obchod)
-#    #  end
-#    #end
-#  end
-#end
-
 #na doplnenie potrebneho poctu nul do adresy kvoli formatu tesco URLky
 def format_category id
   dlzka=id.length
   id = "0"*(8-dlzka)+id
 end
 
-  (4..512).each do |id| #kategorie
-    id = format_category id.to_s #
+(4..512).each do |id| #kategorie
+  id = format_category id.to_s #
 
-    puts "Strana cislo #{id}"
-    html = open("http://potravinydomov.itesco.sk/sk-SK/Product/BrowseProducts?taxonomyId=Cat#{id}")
-    doc = Nokogiri::HTML(html, nil, 'UTF-8')
-    posledna = doc.search('.pagination').search('li').last.text.gsub(/Strana:(?<foo>\d)z\d/, '\k<foo>').to_i
+  puts "Kategoria: #{id}"
+  html = open("http://potravinydomov.itesco.sk/sk-SK/Product/BrowseProducts?taxonomyId=Cat#{id}")
+  doc = Nokogiri::HTML(html, nil, 'UTF-8')
+  posledna = doc.search('.pagination').search('li').last.text.gsub(/Strana:(?<foo>\d)z\d/, '\k<foo>').to_i
 
-    page=1
+  page=1
 
-    while (page <= posledna) do
-      puts "a v nej sme na #{page}"
-      html2= open("http://potravinydomov.itesco.sk/sk-SK/Product/BrowseProducts?taxonomyID=Cat#{id}&pageNo=#{page}")
-      doc2 = Nokogiri::HTML(html2, nil, 'UTF-8')
+  while (page <= posledna) do #pagination v konkretnej kategorii
+    puts "Strana: #{page}"
+    html2= open("http://potravinydomov.itesco.sk/sk-SK/Product/BrowseProducts?taxonomyID=Cat#{id}&pageNo=#{page}")
+    doc2 = Nokogiri::HTML(html2, nil, 'UTF-8')
 
-      doc2.search('.t.product').each do |produkt|
-        meno = produkt.search('h2 a').text.gsub('...','')
+    doc2.search('.t.product').each do |produkt|
 
-        cena = produkt.search('.price').text.split(' ')
+      meno = produkt.search('h2 a').text.gsub('...','')
+      cena = produkt.search('.price').text.split(' ')
+      platnostDo = produkt.search('.promoUntil').text
+      platnostDo.sub!(/.*?(?=\d)/im, '')
 
-        #obchod = "Tesco"
-        platnostDo = produkt.search('.promoUntil').text
-        platnostDo.sub!(/.*?(?=\d)/im, "")
-
-        puts meno.gsub!('\r', '')
-        puts cena[0] +" "+ platnostDo[0..-3] #+" "+obchod
-        puts ""
-      end
-      page +=1
+      puts meno #\\r ??
+      puts cena[0] +' '+ platnostDo[0..-3] #+' '+obchod=Tesco
+      puts ''
     end
+    page +=1
   end
+end
 # subject.save
 #  end
 
